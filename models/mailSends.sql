5b51aa089b8b098128ed46f80a25bfc2769d0a86
山下 康佑 <directus.yamashita@gmail.com>
2024-07-19 05:59:16 +0000

    240719

--- models/mailSends.sql
+++ models/mailSends.sql
@@ -1,15 +1,7 @@
 SELECT
     name
-<<<<<<< HEAD
-    ,'yamashita.kosuke@directus.ne.jp' AS e-mail
-FROM
-    sixth-utility-407301.test_d_won.customer_master
-where
-    e-mail = 'yamashita.kosuke@directus.ne.jp'
-=======
     ,e_mail
 FROM
     sixth-utility-407301.test_d_won.customer_master
 where
     e_mail = 'directus.yamashita@gmail.com'
->>>>>>> 2d1e821bdd23f1fafbc13573ecb6d6b79e0cd981
